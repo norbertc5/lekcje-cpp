6007828209cfda8a0c0f8d430b293464f85a7715
norbertc5 <113235406+norbertc5@users.noreply.github.com>
2023-12-04 15:33:31 +0100

    Dynamiczna alokacja ale troche nie działa

---
+++ dynamicznaAlokacja/main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    int *p = NULL;
+    p = new int[5];
+    int n = 0;
+
+    while(true)
+    {
+        cout << "--MENU--" << endl;
+        cout << "1. Dodaj element." << endl;
+        cout << "2. Wstaw element." << endl;
+        cout << "3. Usun element." << endl;
+        cout << "4. Wyswietl." << endl;
+        cout << "5. Wyjdz." << endl;
+
+        int inp = 0;
+        cin >> inp;
+
+        switch(inp)
+        {
+            case 1:
+            {
+                int x = 0;
+                cout << "Podaj wartosc: ";
+                cin >> x;
+                int *t = new int[n + 1];
+
+                for(int i = 0; i < n; i++)
+                {
+                    t[i] = p[i];
+                }
+                t[n] = x;
+                delete[] p;
+                p = t;
+                n++;
+                break;
+            }
+            case 2:
+                {
+                    break;
+                }
+            case 3:
+                {
+                    int x = 0;
+                    cout << "Podaj element do usuniecia (od 0 do n-1): ";
+                    cin >> x;
+                    int *t = new int[n - 1];
+
+                    for(int i = 0; i < n; i++)
+                    {
+                        if(i == x)
+                        {
+                            t[i] = p[i + 1];
+                            //i++;
+                            continue;
+                        }
+                        else
+                            t[i] = p[i];
+                    }
+                    t[n] = x;
+                    delete[] p;
+                    p = t;
+                    n--;
+                    break;
+                }
+            case 4:
+                {
+                    for(int i = 0; i < n; i++)
+                    {
+                        cout << p[i]<<" ";
+                    }
+                    cout<<endl;
+                    system("pause");
+                    break;
+                }
+            case 5: return 0; break;
+            default: break;
+        }
+        system("CLS");
+    }
+
+    return 0;
+}
+
